5fa35fa260f116d52851c3b7b1de366747a5757f
Yasmin David <32792282+Yasminda19@users.noreply.github.com>
2020-06-24 20:53:22 +0700

    yes

---
+++ pso_multi_robotsearch_UAS.py
@@ -0,0 +1,45 @@
+import numpy as np
+import pyswarms as ps
+from pyswarms.utils.search import RandomSearch
+from pyswarms.utils.functions import single_obj as fx
+from pyswarms.utils.plotters import plot_cost_history, plot_contour, plot_surface
+import matplotlib.pyplot as plt
+from pyswarms.utils.plotters.formatters import Mesher, Designer
+
+import random
+
+def cost_function(xnew):
+
+    v = 1
+    x = 1
+    w = 2.0
+    pw = random()
+    nw = random()
+    vnew = (w*v) + pw * (xnew - x) + nw * (xnew - x)
+    xnew = x + vnew
+
+    return xnew
+
+# Set-up hyperparameters
+options = {'c1': 0.5, 'c2': 0.3, 'w':0.9, 'k': 2, 'p': 2}
+
+# Call instance of PSO
+optimizer = ps.single.LocalBestPSO(n_particles=20, dimensions=2, options=options)
+
+# Perform optimization
+best_cost, best_pos = optimizer.optimize(fx.sphere, iters=1000)
+
+#plot cost history
+plot_cost_history(optimizer.cost_history)
+plt.show()
+
+m = Mesher(func=fx.sphere)
+
+# Make animation
+animation = plot_contour(pos_history=optimizer.pos_history,
+                         mesher=m,
+                         mark=(0.0,0.0))
+
+# Simpan gif dalam jupyternotebook
+animation.save('plot0.gif', writer='imagemagick', fps=10)
+Image(url='plot0.gif')
